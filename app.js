a88037047f7a90a3cd3154ae4679877ad77aae24
Rahul Shinde <82095408+rahulshinders@users.noreply.github.com>
2023-11-12 15:19:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 (async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/myDB");
+    await mongoose.connect(uri);
     console.log("Connected successfully!");
   } catch (error) {
     console.error("Connection error:", error);
